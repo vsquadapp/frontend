8084d9a47c26854ed4386b87430a9aa1964bcb18
Lucas Paiva <lukspaiva95@gmail.com>
2021-04-12 22:13:11 -0300

    correcao link do catalogo

--- src/utils/linkGenerator.js
+++ src/utils/linkGenerator.js
@@ -1,5 +1,5 @@
 export default {
   getProductLink(productId, storeName) {
-    return `${window.location.origin}/#/store/${storeName}/product/${productId}`;
+    return `${window.location.origin}/store/${storeName}/product/${productId}`;
   }
 };
